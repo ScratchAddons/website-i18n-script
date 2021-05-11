c403ef84f9aef1860dbdda779f936c9369cc332b
Hans5958 <email5958.1@gmail.com>
2021-05-11 20:52:35 +0700

    Use simple-git instead of child_process

--- src/src/git-commit-all-and-push.js
+++ src/src/git-commit-all-and-push.js
@@ -1,9 +1,11 @@
-const child_process = require("child_process")
+const simpleGit = require("simple-git").default
+
+const git = simpleGit("./")
 
 module.exports = (commitMessage, gitEmail, gitName) => {
-	child_process.execSync("git", ["config", "user.email", gitEmail])
-	child_process.execSync("git", ["config", "user.name", gitName])
-	child_process.execSync("git", ["add", "-A"])
-	child_process.execSync("git", ["commit", "-m", commitMessage])
-	child_process.execSync("git", ["push", "origin", "master"])
+	git.addConfig("user.email", gitEmail)
+	git.addConfig("user.name", gitName)
+	git.add(".")
+	git.commit(commitMessage)
+	git.push("origin", "master")
 }
